bc76771cf6e182b29b6b888d7bdf876c53182c1a
MichFig <chellefig17@gmail.com>
2021-03-19 11:21:03 -0400

    Intensity color map

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiemVybmFjaCIsImEiOiJja21mMnp6cm0xMGJvMnZwN2J3NTgxaHR4In0.LYgfV4hV4F8cDEQXQ4gTBw";
--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -7,14 +7,14 @@ var map = L.map("mapid", {
 });
 
 // We create the tile layer that will be the background of our map.
-// Light Mode"
+// "Light Mode"
 var lightmap = L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: "mapbox/streets-v8",
   accessToken: API_KEY
 }
 );
@@ -23,7 +23,7 @@ var lightmap = L.tileLayer(
 lightmap.addTo(map);
 
 // Here we make an AJAX call that retrieves our earthquake geoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function (data) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function (data) {
   // This function returns the style data for each of the earthquakes we plot on
   // the map. We pass the magnitude of the earthquake into two separate functions
   // to calculate the color and radius.
@@ -41,18 +41,20 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // This function determines the color of the marker based on the magnitude of the earthquake.
   function getColor(depth) {
     switch (true) {
-      case depth > 90:
-        return "#ea2c2c";
-      case depth > 70:
-        return "#ea822c";
-      case depth > 50:
-        return "#ee9c00";
-      case depth > 30:
-        return "#eecc00";
-      case depth > 10:
-        return "#d4ee00";
-      default:
-        return "#98ee00";
+      case depth > 139:
+        return "#C80000";
+      case depth > 75:
+        return "#FF9100";
+      case depth > 40:
+        return "#7AFF93";
+      case depth > 6.2:
+        return "#7DFFFF";
+      case depth > 2.8:
+        return "#A0E6FF";
+      case depth > 0.03:
+        return "#BFCCFF";
+      case depth < 0.05:
+        return "#FFFFFF";
     }
   }
   // This function determines the radius of the earthquake marker based on its magnitude.
@@ -90,14 +92,16 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // Then add all the details for the legend
   legend.onAdd = function () {
     var div = L.DomUtil.create("div", "info legend");
-    var grades = [-10, 10, 30, 50, 70, 90];
+    var grades = [ -0.05, 0.03,  2.8, 6.2, 40, 75, 139];
     var colors = [
-      "#98ee00",
-      "#d4ee00",
-      "#eecc00",
-      "#ee9c00",
-      "#ea822c",
-      "#ea2c2c"
+      
+      "#C80000",
+      "#FF9100",
+      "#7AFF93",
+      "#7DFFFF",
+      "#A0E6FF",
+      "#BFCCFF",
+      "#FFFFFF"
     ];
     // Looping through our intervals to generate a label with a colored square for each interval.
     for (var i = 0; i < grades.length; i++) {
