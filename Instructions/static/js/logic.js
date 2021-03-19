8ffad404e238cec802e4dc249aef937f4d7d43cc
MichFig <chellefig17@gmail.com>
2021-03-19 09:33:10 -0400

    Initial script

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -1,47 +1,29 @@
-// We create the tile layer that will be the background of our map.
-
-// Light Mode"
-var lightmap = L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-  {
-    attribution:
-      "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/lisatellite-v9",
-    accessToken: API_KEY
-  }
-);
-// Dark Mode
-var darkmap = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/satellite-v9",
-      accessToken: API_KEY
-    }
-  );
-
-
-// mapbox/streets-v11
-
-
-// We create the map object with options.
+// We create the map 
 var map = L.map("mapid", {
   center: [
     40.7, -94.5
   ],
   zoom: 3
 });
+
+// We create the tile layer that will be the background of our map.
+// Light Mode"
+var lightmap = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}
+);
+
 // Then we add our 'lightmap' tile layer to the map.
 lightmap.addTo(map);
+
 // Here we make an AJAX call that retrieves our earthquake geoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function (data) {
   // This function returns the style data for each of the earthquakes we plot on
   // the map. We pass the magnitude of the earthquake into two separate functions
   // to calculate the color and radius.
@@ -59,18 +41,18 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // This function determines the color of the marker based on the magnitude of the earthquake.
   function getColor(depth) {
     switch (true) {
-    case depth > 90:
-      return "#ea2c2c";
-    case depth > 70:
-      return "#ea822c";
-    case depth > 50:
-      return "#ee9c00";
-    case depth > 30:
-      return "#eecc00";
-    case depth > 10:
-      return "#d4ee00";
-    default:
-      return "#98ee00";
+      case depth > 90:
+        return "#ea2c2c";
+      case depth > 70:
+        return "#ea822c";
+      case depth > 50:
+        return "#ee9c00";
+      case depth > 30:
+        return "#eecc00";
+      case depth > 10:
+        return "#d4ee00";
+      default:
+        return "#98ee00";
     }
   }
   // This function determines the radius of the earthquake marker based on its magnitude.
@@ -84,20 +66,20 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // Here we add a GeoJSON layer to the map once the file is loaded.
   L.geoJson(data, {
     // We turn each feature into a circleMarker on the map.
-    pointToLayer: function(feature, latlng) {
+    pointToLayer: function (feature, latlng) {
       return L.circleMarker(latlng);
     },
     // We set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
     // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
-    onEachFeature: function(feature, layer) {
+    onEachFeature: function (feature, layer) {
       layer.bindPopup(
         "Magnitude: "
-          + feature.properties.mag
-          + "<br>Depth: "
-          + feature.geometry.coordinates[2]
-          + "<br>Location: "
-          + feature.properties.place
+        + feature.properties.mag
+        + "<br>Depth: "
+        + feature.geometry.coordinates[2]
+        + "<br>Location: "
+        + feature.properties.place
       );
     }
   }).addTo(map);
@@ -106,7 +88,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     position: "bottomright"
   });
   // Then add all the details for the legend
-  legend.onAdd = function() {
+  legend.onAdd = function () {
     var div = L.DomUtil.create("div", "info legend");
     var grades = [-10, 10, 30, 50, 70, 90];
     var colors = [
@@ -120,7 +102,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     // Looping through our intervals to generate a label with a colored square for each interval.
     for (var i = 0; i < grades.length; i++) {
       div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
-      + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
     }
     return div;
   };
